dab5f9ff2c7989f0736435dab811ef41a5294ee9
Ali Weinberg <aliweinberg@Ali-Weinbergs-MacBook-Pro.local>
2017-02-22 01:56:50 -0500

    unit ten assignment

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var CardThree = "king"; 
 var CardFour = "king";
+
+
+/*if(cardOne === cardTwo){
+	alert("You found a match!"); 
+} else {
+	alert("Sorry, try again.");
+}
+*/
+
+
+var cards = ["queen", "queen", "king", "king"]; 
+function createCards(){
+	var gameBoard = document.getElementById('game-board');
+	for (var i=0; i<cards.length; i++){
+		var cardElement = document.createElement ('div');
+		cardElement.innerHTML = '<div class="card"></div>'; 
+		gameBoard.appendChild(cardElement); 
+		
+	}
+}
+createCards ()
+
+
