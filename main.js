72875b021c4eb2d665fe7b75a028716105efd469
Ali Weinberg <aliweinberg@Ali-Weinbergs-MacBook-Pro.local>
2017-02-26 22:54:21 -0500

    final

--- main.js
+++ main.js
@@ -15,15 +15,45 @@ var CardFour = "king";
 //adding comment
 
 var cards = ["queen", "queen", "king", "king"]; 
-function createCards(){
+var cardsInPlay = []
+var allCards = []
+
+function isMatch (cardsToCheck){
+
+	if(cardsToCheck[0] === cardsToCheck[1]){
+		alert ("It's your day!"); 
+	}
+	else{
+		alert ("NOT TODAY!!!!")
+	}
+	for (var i=0; i<allCards.length; i++){
+		allCards[i].innerHTML=""
+	}	
+}
+
+function isTwoCards () {
+	var cardType = this.getAttribute('data-card')
+	cardsInPlay.push(cardType) // cardType = king or queen
+	var template = '<img src="'+cardType+'.png" />';
+	this.innerHTML = template;
+	if (cardsInPlay.length === 2){
+		isMatch(cardsInPlay)
+		cardsInPlay = []; 
+	}
+}
+
+
+function createBoard(){ 
 	var gameBoard = document.getElementById('game-board');
 	for (var i=0; i<cards.length; i++){
 		var cardElement = document.createElement ('div');
-		cardElement.innerHTML = '<div class="card"></div>'; 
+		cardElement.setAttribute ('class', 'card');
+		cardElement.setAttribute ('data-card',cards[i]); 
 		gameBoard.appendChild(cardElement); 
-		
+		cardElement.addEventListener ("click", isTwoCards)
 	}
+	allCards = document.getElementsByClassName('card')
 }
-createCards ()
+createBoard ()
 
 
